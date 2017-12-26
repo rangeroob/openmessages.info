9801c9b597de85326d3bf1ce609ef5a00ed13e83
derekviera <ma.dmviera01@gmail.com>
2017-12-26 15:25:01 -0500

    Add tests

--- test/test_open-messages.rb
+++ test/test_open-messages.rb
@@ -31,8 +31,10 @@ scope do
   @blacklist_password = '<small> * The password provided is blacklisted </small>'
   @used_username = '<small> * Username already in use </small>'
   test 'SignupError' do
+    # password is on blacklist
     post '/message/signup', 'username' => 'a', 'password' => 'aaaaaaaaaa'
     assert last_response.body.include?(@blacklist_password)
+    # user already exists
     post '/message/signup', 'username' => 'johndoe', 'password' => 'aaaaaaaa'
     assert last_response.body.include?(@used_username)
   end
@@ -42,13 +44,29 @@ scope do
     get '/message/user/johndoe'
     assert last_response.ok?
     assert_equal 200, last_response.status
+    get '/message/user/janedoe'
+    follow_redirect!
+    assert_equal 200, last_response.status
   end
 end
 scope do
   test 'DeleteMessage' do
-    delete'/message/delete', 'uuid' => 'x', 'username' => 'abc', 'password' => 'abc'
+    # user does not exist
+    delete '/message/delete', 'uuid' => 'a', 'username' => 'abc', 'password' => 'abc'
     assert_equal 500, last_response.status
-    delete '/message/delete', 'uuid' => 'a', 'username' => 'johndoe', 'password' => 'jonhdoe1'
+    # user exists but wrong password
+    delete '/message/delete', 'uuid' => 'a', 'username' => 'johndoe', 'password' => 'a'
+    assert_equal 404, last_response.status
+    # user exists and correct password
+    delete '/message/delete', 'uuid' => 'a', 'username' => 'johndoe', 'password' => 'johndoe1'
+    assert_equal 200, last_response.status
+  end
+end
+scope do
+  test 'PutMessage' do
+    put '/message/put', 'username' => 'johndoe', 'password' => 'johndoe1', 'textarea' => '#Hello World'
+    follow_redirect!
+    assert last_response.body.include?('Hello World')
     assert_equal 200, last_response.status
   end
 end
